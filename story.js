bcfa20089198432e5bff459babb681012e8db016
bmoreini <42300498+bmoreini@users.noreply.github.com>
2019-04-10 15:13:15 -0400

    Commented

--- story.js
+++ story.js
@@ -9,14 +9,15 @@ const base_url = 'appb32htKfdksUMzU';
 function getScene(record_id) {
   // this requires a record ID when called from setup function
   const url = `https://api.airtable.com/v0/${base_url}/scenes/${record_id}?api_key=${key}`;
-  // Make GET request to AirTable base using the url.
-  $.ajax({ url: url, type: 'GET' })
+  // Make ajax GET request to AirTable base using the url.
+  $.ajax({ url: url, type: 'GET' }) 
+  // this is jQuery (a JavaScript library - note the $)
+  // https://www.w3schools.com/jquery/ajax_ajax.asp
   // Wait for data to be returned. 
   .done(function (data) {
-      // Once AJAX request returns data, we destructure
-      // it and store it in variables.
+      // Once AJAX request returns data, we destructure it and store it in variables.
       let choices = [];
-      let {title, story} = data.fields;
+      let {title, story} = data.fields; // these are JSON fields
       // Don't bother if the scene doesn't have any choices.
       if (data.fields.choices) {
         // Collect AirTable queries for every choice in scene into an array.
@@ -26,18 +27,20 @@ function getScene(record_id) {
             type: 'GET'
           }));
         }
-        // Use Promise.all() to wait until every query in the array
-        // has been returned before proceeding.
+        // Use Promise.all() to wait until every query in the array has been returned before proceeding.
         Promise.all(choices)
           .then(function (data) {
             let targetArray = [];
             for (let idx = 0; idx < data.length; idx++) {
-              // Destructure the necessary fields.
-              // targets is an array
+              // Destructure the necessary fields and push them into targetArray
               let { choice, targets } = data[idx].fields;
+			  // look at our scenes data 
+			  console.log("Choices after promise"+JSON.stringify(choices));
               targetArray.push({ choice: choice, target: targets[0] });
             }
             displayStory(story);
+			 // look at our choices data
+			console.log("targetArray"+JSON.stringify(targetArray));
 			setOptions(targetArray);
           })
           .catch(function (err) {
